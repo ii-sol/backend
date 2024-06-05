3ca20fa8ae8f8bba9f668611d4820278b163bcaa
NOEL-code <junusong12@naver.com>
2024-06-05 15:24:06 +0900

    프로젝트 구조 변경

--- server1/src/main/java/sinhan/server1/Server1Application.java
+++ server1/src/main/java/sinhan/server1/Server1Application.java
@@ -1,13 +1,33 @@
 package sinhan.server1;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Server1Application {
+public class Server1Application implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public Server1Application(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Server1Application.class, args);
     }
 
+    @Bean
+    public Queue queue(@Value("${rabbitmq.queue}") String queueName) {
+        return new Queue(queueName, false);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        rabbitTemplate.convertAndSend("hello", "Hello from Producer!");
+    }
 }
