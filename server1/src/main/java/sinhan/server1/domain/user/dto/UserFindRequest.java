fe89a282d4ca43a819756085a814a21f0c1e1c9e
ye-s-rin <pyr53540@gmail.com>
2024-06-07 14:17:41 +0900

    fix: add @Pattern @NotBlank

--- server1/src/main/java/sinhan/server1/domain/user/dto/UserFindRequest.java
+++ server1/src/main/java/sinhan/server1/domain/user/dto/UserFindRequest.java
@@ -1,5 +1,7 @@
 package sinhan.server1.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,7 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserFindRequest {
 
+    @Pattern(regexp = "^(\\d{3})-(\\d{3,4})-(\\d{4})$", message = "전화번호 형식이 맞지 않습니다.")
+    @NotBlank(message = "전화번호를 입력해주세요.")
     private String phoneNum;
+    @Pattern(regexp = "^\\d{6}$", message = "비밀번호는 6자리 숫자입니다.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String accountInfo;
 
 }
