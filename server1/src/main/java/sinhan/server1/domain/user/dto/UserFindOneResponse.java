bff72e94307156ae8fc682b72f7438976b643bcd
ye-s-rin <pyr53540@gmail.com>
2024-06-07 14:10:08 +0900

    feat: UserFindOneResponse

---
+++ server1/src/main/java/sinhan/server1/domain/user/dto/UserFindOneResponse.java
@@ -0,0 +1,24 @@
+package sinhan.server1.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Component
+public class UserFindOneResponse {
+
+    private int id;
+    private String phoneNum;
+    private String name;
+    private String birthdate;
+    private String accountInfo;
+    private int role;
+    private int profileId;
+
+}
