9106b82275c62825a4b4dfc13737dfae7ace8790
ye-s-rin <pyr53540@gmail.com>
2024-06-07 12:41:53 +0900

    docs: SQL

--- server1/server1.sql
+++ server1/server1.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `user` (
-    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `phone_num` VARCHAR(20) NOT NULL UNIQUE,
-    `name` VARCHAR(255) NOT NULL,
-    `birthdate` DATE NOT NULL,
-    `account_info` VARCHAR(6) NOT NULL,
-    `role` TINYINT NOT NULL,
-    `profile_id` TINYINT NOT NULL DEFAULT 1
+CREATE TABLE user (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    phone_num VARCHAR(20) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    birthdate DATE NOT NULL,
+    account_info VARCHAR(6) NOT NULL,
+    role TINYINT UNSIGNED NOT NULL,
+    profile_id TINYINT NOT NULL DEFAULT 1
 );
