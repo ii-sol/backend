645c08803be02454db49e2e1fe07fee2a2b31253
ye-s-rin <pyr53540@gmail.com>
2024-06-10 08:15:52 +0900

    feat: (WIP)

--- server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
+++ server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
@@ -1,4 +1,29 @@
 package sinhan.server1.domain.user.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sinhan.server1.domain.user.dto.UserFindOneResponse;
+import sinhan.server1.domain.user.dto.UserFindRequest;
+import sinhan.server1.domain.user.service.UserService;
+import sinhan.server1.global.utils.ApiUtils;
+
+import static sinhan.server1.global.utils.ApiUtils.success;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ApiUtils.ApiResult<UserFindOneResponse> login(@Valid @RequestBody UserFindRequest userFindRequest){
+        return success(userService.login(userFindRequest));
+    }
 }
--- server1/src/main/java/sinhan/server1/domain/user/repository/UserRepository.java
+++ server1/src/main/java/sinhan/server1/domain/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package sinhan.server1.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sinhan.server1.domain.user.entity.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByPhoneNumAndAccountInfo(String phoneNum, String accountInfo);
