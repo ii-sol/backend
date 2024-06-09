dceca55e5d4ea55eb5641ecede85cb161b4d52e0
yerin <pyr53540@gmail.com>
2024-06-09 21:53:52 +0900

    refactor: createJwt

--- server1/src/main/java/sinhan/server1/global/utils/JwtService.java
+++ server1/src/main/java/sinhan/server1/global/utils/JwtService.java
@@ -13,35 +13,25 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Service
 public class JwtService {
 
-    private static final long EXPIRATION_TIME = 1800 * 1000; // 30분
+    private static final long EXPIRATION_TIME = 1800 * 1000; // 30 minutes
     private static final String TOKEN_TYPE = "JWT";
 
-    private String createJwt(Map<String, Object> claims) {
+    public String createParentJwt(String phoneNum, Map<String, String> children) {
+        return createJwt(phoneNum, children, "parent");
+    }
 
+    public String createChildJwt(String phoneNum, Map<String, String> parents) {
+        return createJwt(phoneNum, parents, "child");
+    }
+
+    private String createJwt(String phoneNum, Map<String, String> claims, String userType) {
         Date now = new Date();
         return Jwts.builder()
-            .setClaims(claims)
+            .claim("phoneNum", phoneNum)
+            .claim(userType, claims)
             .setIssuedAt(now)
             .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
             .signWith(SignatureAlgorithm.HS256, Secret.JWT_SECRET_KEY)
             .compact();
-    }
-
-    public String createParentJwt(String phoneNum, Map<String, String> children) {
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("phoneNum", phoneNum);
-        claims.put("children", children);
-        claims.put("type", TOKEN_TYPE);
-        return createJwt(claims);
-    }
-
-    public String createChildJwt(String phoneNum, Map<String, String> parents) {
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("phoneNum", phoneNum);
-        claims.put("parents", parents);
-        claims.put("type", TOKEN_TYPE);
-        return createJwt(claims);
     }
 }
