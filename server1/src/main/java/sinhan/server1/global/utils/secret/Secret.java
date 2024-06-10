439a866e029170741ac1147cfb87eaa2c3947783
ye-s-rin <pyr53540@gmail.com>
2024-06-10 13:43:35 +0900

    refactor: getJwtKey()

--- server1/src/main/java/sinhan/server1/global/utils/secret/Secret.java
+++ server1/src/main/java/sinhan/server1/global/utils/secret/Secret.java
@@ -1,5 +1,16 @@
 package sinhan.server1.global.utils.secret;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
 public class Secret {
-    public static String JWT_SECRET_KEY = "test";
+
+    private static final String JWT_SECRET_KEY = "test";
+
+    public static SecretKey getJwtKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
