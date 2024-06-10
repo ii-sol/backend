83e2e61ff844d8d1db1bbe88f4d4f604d0696f89
ye-s-rin <pyr53540@gmail.com>
2024-06-10 14:28:07 +0900

    fix: @SpringBootApplication(exclude={SecurityAutoConfiguration.class})

--- server1/src/main/java/sinhan/server1/Server1Application.java
+++ server1/src/main/java/sinhan/server1/Server1Application.java
@@ -36,8 +36,9 @@ package sinhan.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Server1Application {
 
     public static void main(String[] args) {
