b502bb48887cf953f7f2b4d50be8b5bb8f77d105
ye-s-rin <pyr53540@gmail.com>
2024-06-10 10:17:12 +0900

    fix: getUser (WIP)

--- server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
+++ server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
@@ -9,6 +9,9 @@ import sinhan.server1.global.utils.ApiUtils;
 import sinhan.server1.global.utils.JwtService;
 import sinhan.server1.global.utils.exception.AuthException;
 
+import java.util.List;
+import java.util.Map;
+
 import static sinhan.server1.global.utils.ApiUtils.error;
 
 @Slf4j
@@ -20,12 +23,22 @@ public class UserController {
     private UserService userService;
     private JwtService jwtService;
 
-    @GetMapping("")
-    public ApiUtils.ApiResult getUser(){
+    @GetMapping("/{id}")
+    public ApiUtils.ApiResult<String> getUser(@PathVariable("id") int id) throws AuthException {
 //        try {
-//            int userId = jwtService.getUserInfo().get("userId");
+//            Map<String, Object> userInfo = jwtService.getUserInfo();
+//            if ((int) userInfo.get("userId") != id) {
+//                String familyType = userInfo.get("parents") != null ? "parents" : "children";
+//                if (familyType.equals("parents")) {
+//                    if(userInfo.get("parents").keySet()==id){
+//                            break;
+//                } else {
+//                    userInfo.get("children");
+//                }
+//            }
+//
 //        } catch (AuthException e) {
-//            throw new AuthException("INVALID_JWT");
+//            throw new AuthException("");
 //        }
 
         return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
