f6a72f7090fb8f4e6f9d58bb3c4564cbefdc3fb3
ye-s-rin <pyr53540@gmail.com>
2024-06-10 15:19:05 +0900

    feat: updateUser (test version)

--- server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
+++ server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
@@ -2,9 +2,11 @@ package sinhan.server1.domain.user.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sinhan.server1.domain.auth.entity.FamilyInfo;
 import sinhan.server1.domain.user.dto.UserFindOneResponse;
+import sinhan.server1.domain.user.dto.UserUpdateRequest;
 import sinhan.server1.domain.user.service.UserService;
 import sinhan.server1.global.utils.ApiUtils;
 import sinhan.server1.global.utils.JwtService;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static sinhan.server1.global.utils.ApiUtils.error;
 import static sinhan.server1.global.utils.ApiUtils.success;
 
 @Slf4j
@@ -26,7 +29,7 @@ public class UserController {
     private JwtService jwtService;
 
     @GetMapping("/{id}")
-    public ApiUtils.ApiResult<UserFindOneResponse> getUser(@PathVariable("id") int id) throws AuthException {
+    public ApiUtils.ApiResult getUser(@PathVariable("id") int id) throws AuthException {
 //        Map<String, Object> userInfo = jwtService.getUserInfo();
 //        if ((int) userInfo.get("userId") != id) {
 //            List<FamilyInfo> familyInfo = (List<FamilyInfo>) userInfo.get("familyInfo");
@@ -42,7 +45,17 @@ public class UserController {
 //        }
 
         UserFindOneResponse user = userService.getUser(id);
-        return success(user);
+        return user.getId() == id ? success(user) : error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
     }
 
+    @PutMapping("")
+    public ApiUtils.ApiResult updateUser(@RequestBody UserUpdateRequest userUpdateRequest) throws AuthException {
+//        Map<String, Object> userInfo = jwtService.getUserInfo();
+//        if ((int) userInfo.get("userId") == id)
+
+        int id = 1; // jwt 연결하기전까지 임시 데이터 사용
+        userUpdateRequest.setId(id);
+        UserFindOneResponse user = userService.updateUser(userUpdateRequest);
+        return user != null ? success(user) : error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
+    }
 }
