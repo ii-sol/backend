e403bbeeab8de832d69dbdfac1866a1e1597a2b4
ye-s-rin <pyr53540@gmail.com>
2024-06-10 13:13:36 +0900

    fix: remove @repository

--- server1/src/main/java/sinhan/server1/domain/user/repository/UserRepository.java
+++ server1/src/main/java/sinhan/server1/domain/user/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package sinhan.server1.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import sinhan.server1.domain.user.entity.User;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByPhoneNumAndAccountInfo(String phoneNum, String accountInfo);
-    Optional<User> findById(int id);
 }
