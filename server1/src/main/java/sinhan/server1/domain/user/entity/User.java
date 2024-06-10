9793fa8849cbef514754d55fd9bd7c2470e9089d
yerin <pyr53540@gmail.com>
2024-06-10 23:54:02 +0900

    refactor: db option

--- server1/src/main/java/sinhan/server1/domain/user/entity/User.java
+++ server1/src/main/java/sinhan/server1/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package sinhan.server1.domain.user.entity;
 
 import jakarta.persistence.*;
 
+import java.sql.Date;
 import lombok.*;
 import sinhan.server1.domain.user.dto.UserFindOneResponse;
 
@@ -9,24 +10,33 @@ import sinhan.server1.domain.user.dto.UserFindOneResponse;
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "phone_num", nullable = false)
+    @Column(name = "phone_num", nullable = false, unique = true)
     private String phoneNum;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
-    private String birthdate;
+    private Date birthdate;
     @Column(name = "account_info", nullable = false)
     private String accountInfo;
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TINYINT UNSIGNED")
     private int role;
-    @Column(name = "profile_id", nullable = false)
+    @Column(name = "profile_id", nullable = false, columnDefinition = "TINYINT UNSIGNED DEFAULT 1")
     private int profileId;
+
+    public User(String phoneNum, String name, Date birthdate, String accountInfo, int role,
+        int profileId) {
+        this.phoneNum = phoneNum;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.accountInfo = accountInfo;
+        this.role = role;
+    }
 
     public UserFindOneResponse convertToUserFindOneResponse() {
         return new UserFindOneResponse(id, phoneNum, name, birthdate, accountInfo, role, profileId);
