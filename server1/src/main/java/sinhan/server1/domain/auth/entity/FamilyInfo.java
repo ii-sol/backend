b89c39984f5c7137e5b57d551e1942b54671750f
ye-s-rin <pyr53540@gmail.com>
2024-06-10 10:57:23 +0900

    feat: FamilyInfo Entity

---
+++ server1/src/main/java/sinhan/server1/domain/auth/entity/FamilyInfo.java
@@ -0,0 +1,10 @@
+package sinhan.server1.domain.auth.entity;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class FamilyInfo {
+
+    private int id;
+    private String name;
+}
