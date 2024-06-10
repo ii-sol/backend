6acbbb730ebcca21345f28d3eac1d24fe7933f8a
ye-s-rin <pyr53540@gmail.com>
2024-06-10 13:12:53 +0900

    fix: add @JsonProperty

--- server1/src/main/java/sinhan/server1/domain/user/dto/UserFindOneResponse.java
+++ server1/src/main/java/sinhan/server1/domain/user/dto/UserFindOneResponse.java
@@ -1,5 +1,6 @@
 package sinhan.server1.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +16,14 @@ import sinhan.server1.domain.user.entity.User;
 public class UserFindOneResponse {
 
     private int id;
+    @JsonProperty(value = "phone_num")
     private String phoneNum;
     private String name;
     private String birthdate;
+    @JsonProperty(value = "account_info")
     private String accountInfo;
     private int role;
+    @JsonProperty(value = "profile_id")
     private int profileId;
 
     public static UserFindOneResponse from(User user) {
