998111c570323fac82fcea104913e18c71a5465e
ye-s-rin <pyr53540@gmail.com>
2024-06-11 09:27:55 +0900

    fix: connectFamily

--- server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
+++ server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import sinhan.server1.domain.user.dto.FamilyFindOneResponse;
 import sinhan.server1.domain.user.dto.FamilySaveRequest;
 import sinhan.server1.domain.user.dto.UserFindOneResponse;
 import sinhan.server1.domain.user.dto.UserUpdateRequest;
@@ -11,6 +12,7 @@ import sinhan.server1.domain.user.service.UserService;
 import sinhan.server1.global.utils.ApiUtils;
 import sinhan.server1.global.utils.JwtService;
 import sinhan.server1.global.utils.exception.AuthException;
+import sinhan.server1.global.utils.exception.SqlException;
 
 import static sinhan.server1.global.utils.ApiUtils.error;
 import static sinhan.server1.global.utils.ApiUtils.success;
@@ -56,28 +58,33 @@ public class UserController {
     }
 
     @PostMapping("")
-    public ApiUtils.ApiResult connectFamily(@RequestBody FamilySaveRequest familySaveRequest) {
+    public ApiUtils.ApiResult connectFamily(@RequestBody FamilySaveRequest familySaveRequest) throws SqlException {
         //        Map<String, Object> userInfo = jwtService.getUserInfo();
         //        if ((int) userInfo.get("userId") == id)
         // int role = userInfo.get("role")
 
-        log.info("1. FamilySaveRequest={}", familySaveRequest);
         int id = 2; // jwt 연결하기전까지 임시 데이터 사용
         int role = 1; // jwt 연결하기전까지 임시 데이터 사용
         familySaveRequest.setId(id);
         familySaveRequest.setRole(role);
-        log.info("2. FamilySaveRequest={}", familySaveRequest);
-        boolean isConnected = userService.connectFamily(familySaveRequest);
-        log.info("isConnected={}", isConnected);
-
-        return isConnected ? success(null) : error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
+        try {
+            FamilyFindOneResponse family = userService.connectFamily(familySaveRequest);
+            if (family != null) {
+                return success("가족 관계가 생성되었습니다.");
+            } else {
+                return error("잘못된 사용자 요청입니다.", HttpStatus.BAD_REQUEST);
+            }
+        } catch (SqlException e) {
+            return error("이미 존재하는 가족 관계입니다.", HttpStatus.CONFLICT);
+        }
     }
 
     @DeleteMapping("/{fo}")
-    public ApiUtils.ApiResult disconnectFamily(@PathVariable int fo){
+    public ApiUtils.ApiResult disconnectFamily(@PathVariable int fo) {
         //        Map<String, Object> userInfo = jwtService.getUserInfo();
 
-        int familyId = 2; // jwt 연결하기전까지 임시 데이터 사용
+        int id = 1; // jwt 연결하기전까지 임시 데이터 사용
+        int familyId = 3; // jwt 연결하기전까지 임시 데이터 사용
 
         return null;
     }
--- server1/src/main/java/sinhan/server1/domain/user/service/UserService.java
+++ server1/src/main/java/sinhan/server1/domain/user/service/UserService.java
@@ -3,15 +3,14 @@ package sinhan.server1.domain.user.service;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import sinhan.server1.domain.user.dto.FamilySaveRequest;
-import sinhan.server1.domain.user.dto.UserFindOneResponse;
-import sinhan.server1.domain.user.dto.UserFindRequest;
-import sinhan.server1.domain.user.dto.UserUpdateRequest;
+import sinhan.server1.domain.user.dto.*;
 import sinhan.server1.domain.user.entity.Family;
 import sinhan.server1.domain.user.entity.User;
 import sinhan.server1.domain.user.repository.FamilyRepository;
 import sinhan.server1.domain.user.repository.UserRepository;
+import sinhan.server1.global.utils.exception.SqlException;
 
 import java.util.NoSuchElementException;
 
@@ -26,8 +25,8 @@ public class UserService {
     @Transactional
     public UserFindOneResponse login(UserFindRequest userFindRequest) {
         User loginUser = userRepository.findByPhoneNumAndAccountInfo(
-            userFindRequest.getPhoneNum(),
-            userFindRequest.getAccountInfo()
+                userFindRequest.getPhoneNum(),
+                userFindRequest.getAccountInfo()
         ).orElseThrow(() -> new NoSuchElementException("아이디가 존재하지 않습니다."));
 
         return loginUser.convertToUserFindOneResponse();
@@ -36,7 +35,7 @@ public class UserService {
     @Transactional
     public UserFindOneResponse getUser(int id) {
         User user = userRepository.findById(id)
-            .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
+                .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
 
         return user.convertToUserFindOneResponse();
     }
@@ -44,7 +43,7 @@ public class UserService {
     @Transactional
     public UserFindOneResponse updateUser(UserUpdateRequest userUpdateRequest) {
         User user = userRepository.findById(userUpdateRequest.getId())
-            .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
+                .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
 
         user.setPhoneNum(userUpdateRequest.getPhoneNum());
         user.setName(userUpdateRequest.getName());
@@ -56,11 +55,10 @@ public class UserService {
         return getUser(updatedUser.getId());
     }
 
-    public boolean connectFamily(FamilySaveRequest familySaveRequest) {
+    public FamilyFindOneResponse connectFamily(FamilySaveRequest familySaveRequest) throws SqlException {
         User familyUser = userRepository.findByPhoneNum(familySaveRequest.getPhoneNum())
-            .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
+                .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
 
-        log.info("familyUser.getRole={}", familyUser.getRole());
         if (familySaveRequest.getRole() == familyUser.getRole()) {
             throw new IllegalArgumentException("임시로 사용하는 에러. 부모자식 관계 불일치");
         }
@@ -70,8 +68,10 @@ public class UserService {
         User parents = (familySaveRequest.getRole() == 1) ? meUser : familyUser;
         User child = (familySaveRequest.getRole() == 1) ? familyUser : meUser;
 
-        Family family = familyRepository.save(new Family(parents, child));
-
-        return family.getId() != 0;
+        try {
+            return familyRepository.save(new Family(parents, child)).convertToFamilyFindOneResponse();
+        } catch (DataIntegrityViolationException e) {
+            throw new SqlException("이미 존재하는 가족입니다.");
+        }
     }
 }
