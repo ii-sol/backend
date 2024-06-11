5f846502ddd0a393906c6e80c53b3ac65632dd94
ye-s-rin <pyr53540@gmail.com>
2024-06-11 15:36:53 +0900

    refactor: Annotation

---
+++ server1/src/main/java/sinhan/server1/domain/auth/service/AuthService.java
@@ -0,0 +1,10 @@
+package sinhan.server1.domain.auth.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+}
