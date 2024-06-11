abbb205965344e102d5656d8fd7e0f80be0b344f
ye-s-rin <pyr53540@gmail.com>
2024-06-11 13:52:03 +0900

    feat: error methods

--- server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
+++ server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
@@ -3,22 +3,48 @@ package sinhan.server1.global.utils.exception;
 import static sinhan.server1.global.utils.ApiUtils.error;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sinhan.server1.global.utils.ApiUtils.ApiResult;
 
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.NoSuchElementException;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler//(MethodArgumentNotValidException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ApiResult<String> handleValidationExceptions(AuthException errors) {
-        String errorMessage = errors.getMessage();
+    public ApiResult<String> handleValidationExceptions(AuthException error) {
+        return error(error.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
 
-        log.info("errorMessage.toString={}", errorMessage.toString());
-        return error(errorMessage, HttpStatus.UNAUTHORIZED);
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiResult<String> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException error) {
+        return error(error.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResult<String> handleNoSuchElementException(NoSuchElementException error) {
+        return error(error.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult<String> handleDataIntegrityViolationException(DataIntegrityViolationException error) {
+        return error(error.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResult<String> handleSQLException(SQLException error) {
+        return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
