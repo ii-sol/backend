77426986c31025c4b40bb2be783c7ecad6a6eb37
ye-s-rin <pyr53540@gmail.com>
2024-06-11 15:42:14 +0900

    feat: FamilyInfoResponse

---
+++ server1/src/main/java/sinhan/server1/domain/auth/dto/FamilyInfoResponse.java
@@ -0,0 +1,12 @@
+package sinhan.server1.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FamilyInfoResponse {
+
+    private int id;
+    private String name;
+}
