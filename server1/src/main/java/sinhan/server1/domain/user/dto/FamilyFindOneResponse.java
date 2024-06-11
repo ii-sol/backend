872883989b2b57a5ce159d6c64a531dc580a2b3a
ye-s-rin <pyr53540@gmail.com>
2024-06-11 09:24:30 +0900

    feat: FamilyFindOneResponse

--- server1/src/main/java/sinhan/server1/domain/user/dto/FamilyFindOneResponse.java
+++ server1/src/main/java/sinhan/server1/domain/user/dto/FamilyFindOneResponse.java
@@ -1,14 +1,20 @@
 package sinhan.server1.domain.user.dto;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
 import sinhan.server1.domain.user.entity.User;
 
-@Entity
+@Builder
 @AllArgsConstructor
+@Getter
 public class FamilyFindOneResponse {
 
     private int id;
+    @JsonProperty(value = "parents_id")
     private User parents;
+    @JsonProperty(value = "child_id")
     private User child;
 }
