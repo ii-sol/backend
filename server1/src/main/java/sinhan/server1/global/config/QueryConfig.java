ce0e109a81c305ffd582ff03bb2c469871370c50
ye-s-rin <pyr53540@gmail.com>
2024-06-12 08:53:52 +0900

    refactor: rename Config -> QueryConfig

--- server1/src/main/java/sinhan/server1/global/config/QueryConfig.java
+++ server1/src/main/java/sinhan/server1/global/config/QueryConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class QueryConfig {
 
     @Bean
     public Dotenv dotenv() {
