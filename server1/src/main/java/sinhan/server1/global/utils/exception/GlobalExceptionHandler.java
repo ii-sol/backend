fe690f978dec045c8eaf83467fcb550020e6a09c
ye-s-rin <pyr53540@gmail.com>
2024-06-12 10:15:28 +0900

    feat: handleMethodArgumentNotValidException

--- server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
+++ server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import static sinhan.server1.global.utils.ApiUtils.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -40,5 +41,11 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResult<String> handleSQLException(SQLException error) {
         return error(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResult<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException error) {
+        return error(error.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
