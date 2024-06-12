e59d329d6d7809b27352a88875ac46d271e037a3
yerin <pyr53540@gmail.com>
2024-06-12 21:42:08 +0900

    feat: findByChildId

--- server1/src/main/java/sinhan/server1/domain/user/repository/ScoreRepository.java
+++ server1/src/main/java/sinhan/server1/domain/user/repository/ScoreRepository.java
@@ -1,7 +1,10 @@
 package sinhan.server1.domain.user.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sinhan.server1.domain.user.entity.Score;
 
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
+
+    Optional<Score> findByChildId(int childId);
 }
