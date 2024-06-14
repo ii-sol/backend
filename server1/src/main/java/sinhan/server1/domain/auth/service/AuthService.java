5642a5b0fe13eebb1a0ed699f0a0ef258139eb20
ye-s-rin <pyr53540@gmail.com>
2024-06-14 13:38:45 +0900

    fix: join - PasswordEncoder

--- server1/src/main/java/sinhan/server1/domain/auth/service/AuthService.java
+++ server1/src/main/java/sinhan/server1/domain/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import sinhan.server1.domain.auth.dto.FamilyInfoInterface;
 import sinhan.server1.domain.auth.dto.JoinInfoSaveRequest;
@@ -26,18 +27,18 @@ public class AuthService {
     private UserRepository userRepository;
     private ScoreRepository scoreRepository;
     private FamilyRepository familyRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserFindOneResponse join(JoinInfoSaveRequest joinInfoSaveRequest) {
-        userRepository.save(joinInfoSaveRequest.convertToUser());
-        User joinUser = userRepository.findByPhoneNumAndAccountInfo(
-                joinInfoSaveRequest.getPhoneNum(), joinInfoSaveRequest.getAccountInfo())
-            .orElseThrow(() -> new NoSuchElementException("가입에 실패하였습니다."));
+        userRepository.save(joinInfoSaveRequest.convertToUser(passwordEncoder));
+        User joinUser = userRepository.findByPhoneNum(joinInfoSaveRequest.getPhoneNum())
+                .orElseThrow(() -> new NoSuchElementException("가입에 실패하였습니다."));
 
         if (joinInfoSaveRequest.getRole() == 2) {
             scoreRepository.save(new Score(joinUser));
             Score score = scoreRepository.findByChildId(joinUser.getId())
-                .orElseThrow(() -> new NoSuchElementException("가입에 실패하였습니다."));
+                    .orElseThrow(() -> new NoSuchElementException("가입에 실패하였습니다."));
         }
 
         return joinUser.convertToUserFindOneResponse();
@@ -46,8 +47,8 @@ public class AuthService {
     @Transactional
     public UserFindOneResponse login(@Valid LoginInfoFindRequest loginInfoFindRequest) {
         User loginUser = userRepository.findByPhoneNumAndAccountInfo(
-                loginInfoFindRequest.getPhoneNum(), loginInfoFindRequest.getAccountInfo())
-            .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
+                        loginInfoFindRequest.getPhoneNum(), loginInfoFindRequest.getAccountInfo())
+                .orElseThrow(() -> new NoSuchElementException("사용자가 존재하지 않습니다."));
 
         return loginUser.convertToUserFindOneResponse();
     }
