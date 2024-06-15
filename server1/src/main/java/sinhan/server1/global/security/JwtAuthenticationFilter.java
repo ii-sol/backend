9b5865ae9cad8a53d3dd2f738295b448f5a21cc8
ye-s-rin <pyr53540@gmail.com>
2024-06-15 22:32:59 +0900

    refactor: remove unused parameter in getAuthentication

--- server1/src/main/java/sinhan/server1/global/security/JwtAuthenticationFilter.java
+++ server1/src/main/java/sinhan/server1/global/security/JwtAuthenticationFilter.java
@@ -23,7 +23,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
         if (token != null) {
             try {
-                Authentication authentication = jwtService.getAuthentication(token);
+                Authentication authentication = jwtService.getAuthentication();
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (AuthException e) {
                 throw new RuntimeException(e);
