cc9a42745245378cfcd08a895c52e458dbd960f1
ye-s-rin <pyr53540@gmail.com>
2024-06-15 21:56:24 +0900

    refactor: @NoArgsConstructor

--- server1/src/main/java/sinhan/server1/domain/auth/dto/JwtTokenResponse.java
+++ server1/src/main/java/sinhan/server1/domain/auth/dto/JwtTokenResponse.java
@@ -2,9 +2,11 @@ package sinhan.server1.domain.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JwtTokenResponse {
 
     private String accessToken;
