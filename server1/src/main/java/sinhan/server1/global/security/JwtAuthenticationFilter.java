0dc641e89e895fcb73bd63217c5cf92a52387bd2
ye-s-rin <pyr53540@gmail.com>
2024-06-16 10:49:07 +0900

    refactor: @SneakyThrows

--- server1/src/main/java/sinhan/server1/global/security/JwtAuthenticationFilter.java
+++ server1/src/main/java/sinhan/server1/global/security/JwtAuthenticationFilter.java
@@ -1,33 +1,27 @@
 package sinhan.server1.global.security;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
-import sinhan.server1.global.utils.exception.AuthException;
-
-import java.io.IOException;
 
 @AllArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
     JwtService jwtService;
 
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         String token = jwtService.getAccessToken();
 
         if (token != null) {
-            try {
-                Authentication authentication = jwtService.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (AuthException e) {
-                throw new RuntimeException(e);
-            }
+            Authentication authentication = jwtService.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
