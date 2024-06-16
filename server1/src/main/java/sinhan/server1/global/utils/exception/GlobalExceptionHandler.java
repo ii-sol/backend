cf2cc2b283d652127c0a212b0da38742987f0bdd
ye-s-rin <pyr53540@gmail.com>
2024-06-16 09:30:04 +0900

    refactor: use @RestControllerAdvice

--- server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
+++ server1/src/main/java/sinhan/server1/global/utils/exception/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.core.convert.ConverterNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
