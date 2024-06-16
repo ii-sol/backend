01cf6880b89c55f0783d4c05b8101c0dd12ef378
ye-s-rin <pyr53540@gmail.com>
2024-06-16 09:24:00 +0900

    refactor: remove unused library

--- server1/src/main/java/sinhan/server1/domain/auth/controller/AuthController.java
+++ server1/src/main/java/sinhan/server1/domain/auth/controller/AuthController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
