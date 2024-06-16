6841f9709aaf322f0d4f16780c016036333d3f98
ye-s-rin <pyr53540@gmail.com>
2024-06-16 21:43:57 +0900

    refactor: Parents version

--- server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
+++ server1/src/main/java/sinhan/server1/domain/user/controller/UserController.java
@@ -62,7 +62,7 @@ public class UserController {
         UserInfoResponse userInfo = jwtService.getUserInfo(jwtService.getAccessToken());
 
         if (!isFamilyUser(familySn)) {
-            throw new NoSuchElementException("부모 사용자가 존재하지 않습니다.");
+            throw new NoSuchElementException("아이 사용자가 존재하지 않습니다.");
         }
 
         if (isConnected(familySn)) {
@@ -77,12 +77,12 @@ public class UserController {
     }
 
     private boolean isFamilyUser(long familySn) {
-        // TODO: 부모 사용자 확인 이벤트 등록 - 콜백
+        // TODO: 아이 사용자 확인 이벤트 등록 - 콜백
         return true;
     }
 
     private boolean isConnected(long familySn) {
-        // TODO: 부모 서버 가족 관계 생성 이벤트 등록 - 콜백
+        // TODO: 아이 서버 가족 관계 생성 이벤트 등록 - 콜백
         return true;
     }
 
